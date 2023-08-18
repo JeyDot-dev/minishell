ac36e80d1c4527d2395b7f6559866f4fc1679d59
JeyDot-dev <72892603+JeyDot-dev@users.noreply.github.com>
2023-08-18 16:49:15 +0200

    not important lol

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jsousa-a <jsousa-a@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/17 14:19:49 by jsousa-a          #+#    #+#             */
-/*   Updated: 2023/08/17 19:21:26 by jsousa-a         ###   ########.fr       */
+/*   Updated: 2023/08/18 16:36:32 by jsousa-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,11 @@ int	main(int ac, char **av, char **envp)
 {
 	(void)	ac;
 	(void)	envp;
+	for (char **env = envp; *env != 0; env++)
+  	{
+    	char *thisEnv = *env;
+    	ft_printf("%s\n", thisEnv);
+  	}
 	pwd();
 	cd(av[1]);
 	pwd();
