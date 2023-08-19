06118a381a1c029dd283a1620dc6dfadb68c7d83
JeyDot-dev <72892603+JeyDot-dev@users.noreply.github.com>
2023-08-20 01:33:01 +0200

    just some tests

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: jsousa-a <jsousa-a@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/17 14:19:49 by jsousa-a          #+#    #+#             */
-/*   Updated: 2023/08/18 16:36:32 by jsousa-a         ###   ########.fr       */
+/*   Updated: 2023/08/18 17:50:32 by jsousa-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,22 @@ int	main(int ac, char **av, char **envp)
 {
 	(void)	ac;
 	(void)	envp;
-	for (char **env = envp; *env != 0; env++)
-  	{
-    	char *thisEnv = *env;
-    	ft_printf("%s\n", thisEnv);
-  	}
 	pwd();
 	cd(av[1]);
+	ft_printf("\n");
+//	for (char **env = envp; *env != 0; env++)
+//	{
+//		char *thisEnv = *env;
+//		ft_printf("%s\n", thisEnv);
+//}
+
+	ft_printf("---GET ENV : %s---\n", getenv("HOME"));
+	envp = malloc(1);
+	envp[0] = ft_strdup("PATH=Wapiti");
+	ft_printf("---print str : %s---\n", envp[0]);
+	ft_printf("---GET ENV : %s---\n", getenv("PATH"));
+	pwd();
+	cd(av[2]);
 	pwd();
 	exit(0);
 	return (0);
