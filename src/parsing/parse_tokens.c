7fd7c4bfb411c8fbb196622f1f48803fa72875fa
JeyDot-dev <github.45fle@dralias.com>
2023-10-17 18:50:06 +0200

    deleted TODO and made the parse_tokens comment a little more detailed

--- src/parsing/parse_tokens.c
+++ src/parsing/parse_tokens.c
@@ -6,12 +6,12 @@
 /*   By: jsousa-a <jsousa-a@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 12:18:53 by jsousa-a          #+#    #+#             */
-/*   Updated: 2023/10/17 18:42:29 by jsousa-a         ###   ########.fr       */
+/*   Updated: 2023/10/17 18:49:23 by jsousa-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
+// if no cmd/arg found, t_status = 0, if no such file or dire, t_status = 1
 int	parse_tokens(t_tokens *tokens, t_shell *shell)
 {
 	(void) tokens;
@@ -24,6 +24,6 @@ int	parse_tokens(t_tokens *tokens, t_shell *shell)
 	//						if ^ error repeat last step with next pipe
 	//				open in/out files and set them in t_cmds in/out
 	//				check if out != 1 if it is, out = next pipe
-	//				find cmd and arguments
+	//				find cmd and arguments if no cmd/arg > close fd and set error
 	return (0);
 }
