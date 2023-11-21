3c705c146a898019ca386ee2b0ceb00dcc33744a
JeyDot-dev <github.45fle@dralias.com>
2023-11-21 20:22:19 +0100

    deleted fprintf from signal_handler

--- src/signal_handler.c
+++ src/signal_handler.c
@@ -6,7 +6,7 @@
 /*   By: jsousa-a <jsousa-a@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/29 16:05:48 by jsousa-a          #+#    #+#             */
-/*   Updated: 2023/11/21 19:40:14 by jsousa-a         ###   ########.fr       */
+/*   Updated: 2023/11/21 20:22:02 by jsousa-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "minishell.h"
@@ -31,8 +31,6 @@ void	signal_handler(int sig, siginfo_t *info, void *ucontext)
 	if (sig != SIGINT)
 		return ;
 	g_status = sig;
-	ft_fprintf(2, "info->si_pid: %d\n", info->si_pid);
-	ft_fprintf(2, "ucontext: %p\n", ucontext);
 	if (info->si_pid == 0)
 		return ;
 	ioctl(STDIN_FILENO, TIOCSTI, "\n");
