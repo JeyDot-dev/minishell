8dcb25c69e13ed9234310617f4df05bea7eb79f3
JeyDot-dev <github.45fle@dralias.com>
2023-11-28 07:53:25 +0100

    added cd '-'

--- src/builtin/cd.c
+++ src/builtin/cd.c
@@ -6,13 +6,13 @@
 /*   By: jsousa-a <jsousa-a@student.42lausanne.ch>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/17 18:54:44 by jsousa-a          #+#    #+#             */
-/*   Updated: 2023/11/27 01:01:58 by jsousa-a         ###   ########.fr       */
+/*   Updated: 2023/11/28 07:52:47 by jsousa-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 #include "minishell.h"
 //if cd = 1 then too many args
 //if cd = 2 then HOME not set
+
 int	ft_count_args(char **args)
 {
 	int	i;
@@ -37,6 +37,29 @@ void	update_env_cd(char ***env, char *old_pwd)
 	}
 }
 
+int	cd_underscore(char *path, char ***env)
+{
+	if (!path)
+	{
+		ft_fprintf(2, "cd: OLDPWD not set\n");
+		return (1);
+	}
+	else
+	{
+		if (!chdir(path))
+		{
+			update_env_cd(env, path);
+			ft_printf("%s\n", getvar_data(*env, "PWD"));
+		}
+		else
+		{
+			ft_fprintf(2, "cd: %s: %s\n", path, strerror(errno));
+			return (1);
+		}
+	}
+	return (0);
+}
+
 int	ft_cd(char ***env, char **args)
 {
 	char	*tmp;
@@ -47,8 +70,10 @@ int	ft_cd(char ***env, char **args)
 		ft_fprintf(2, "cd: too many arguments\n");
 		return (1);
 	}
-	if (!args[1] || ((*args)[1] == '~' || (*args)[1] == 0))
+	if (!args[1] || !ft_strncmp(args[1], "~", 2) || *args[1] == 0)
 		tmp = getvar_data(*env, "HOME");
+	else if (!ft_strncmp(args[1], "-", 2))
+		return (cd_underscore(getvar_data(*env, "OLDPWD"), env));
 	else
 		tmp = args[1];
 	if (!tmp)
